17c2d80411e98e16aac6bd57d12b520ec6d43c3e
Hirochika Asai <asai@scyphus.co.jp>
2013-06-30 16:46:47 +0900

    Implemented MBR to load the kernel loader from the boot drive.

--- src/boot/arch/x86_64/diskboot.s
+++ src/boot/arch/x86_64/diskboot.s
@@ -5,6 +5,16 @@
  *      Hirochika Asai  <asai@scyphus.co.jp>
  */
 
+	.set	LOADER_SEG,0x0900	/* Memory where to load kernel loader */
+	.set	LOADER_OFF,0x0000	/*  segment and offset [0900:0000] */
+	.set	LOADER_SIZE,0x8		/* Size of kernel loader in sectors */
+
+	/* Disk information */
+	.set	HEAD_SIZE,18            /* 18 sectors per head/track */
+	.set	CYLINDER_SIZE,2         /* 2 heads per cylinder */
+	.set	NUM_RETRIES,3		/* Number of times to retry to read */
+	.set	ERRCODE_TIMEOUT,0x80	/* Error code: timeout */
+
 	.file	"diskboot.s"
 
 /* Text section */
@@ -19,8 +29,10 @@ start:
 				/* (inc di/si for str ops) */
 	xorw	%ax,%ax		/* %ax=0 */
 /* Setup stack */
+	cli
 	movw	%ax,%ss
 	movw	$start,%sp
+	sti
 /* Setup data segments (%ds=0, %es=0) */
 	movw	%ax,%ds
 	movw	%ax,%es
@@ -29,6 +41,101 @@ start:
 /* Display the welcome message */
 	movw	$msg_welcome,%si	/* %ds:(%si) -> welcome message */
 	call	putstr
+
+/* Read the kernel loader */
+load_loader:
+/* Load 0x8 sectors (4KiB) from the sector next to MBR */
+	pushw	%es
+	movw	$LOADER_SEG,%ax
+	movw	%ax,%es
+	movw	$LOADER_OFF,%bx
+	movb	$LOADER_SIZE,%dh
+	movw	$0x1,%ax		/* from LBA 1 */
+	call	read
+	popw	%es
+/* Jump to the kernel loader */
+	ljmp	$LOADER_SEG,$LOADER_OFF
+
+/* Read %dh sectors starting at LBA (logical block address) %ax on drive %dl
+   into %es:[%bx] */
+read:
+	pushw	%bp		/* Save the base pointer */
+	movw	%sp,%bp		/* Copy the stack pointer to the base pointer */
+/* Save registers */
+	movw	%ax,-2(%bp)
+	movw	%bx,-4(%bp)
+	movw	%cx,-6(%bp)
+	movw	%dx,-8(%bp)
+	/* u16 track:sector -10(%bp) */
+	/* u16 counter -12(%bp) */
+	subw	$12,%sp
+
+/* Convert LBA to CHS */
+	call	lba2chs		/* Convert LBA %ax to CHS (%ch,%dh,%cl) */
+	movw	%cx,-10(%bp)	/* Save %cx to the stack */
+/* Restore %bx */
+	movw	-4(%bp),%bx
+/* Reset counter */
+	xorw	%cx,%cx
+	movw	%cx,-12(%bp)
+read.retry:
+	call	twiddle         /* Display twiddling bar to the user */
+	movw	-8(%bp),%ax	/* Get the saved %dx from the stack */
+	movb	%ah,%al		/*  (Note: same as movb -7(%bp),%al */
+	movb	$0x02,%ah       /* BIOS: Read sectors from drive */
+	movw	-10(%bp),%cx	/* Get the saved %cx from the stack */
+	int	$0x13		/* CHS=%ch,%dh,%cl, drive=%dl, count=%al */
+				/*  to %es:[%bx] (results in %ax,%cf)  */
+	jc	read.fail	/* Fail (%cf=1) */
+
+/* Restore registers */
+	movw	-8(%bp),%dx
+	movw	-6(%bp),%cx
+	movw	-4(%bp),%bx
+	movw	-2(%bp),%ax
+	movw	%bp,%sp		/* Restore the stack pointer and base pointer */
+	popw	%bp
+	ret
+read.fail:
+	movw	-12(%bp),%cx
+	incw	%cx
+	movw	%cx,-12(%bp)
+	cmpw	$NUM_RETRIES,%cx
+	ja	read.error		/* Exceed retries */
+	cmpb	$ERRCODE_TIMEOUT,%ah	/* Timeout? */
+	je	read.retry		/* Yes, then retry */
+read.error:				/* We do not restore the stack */
+	movb	%ah,%al			/* Save error */
+	movw	$hex_error,%di		/* Format it as hex */
+	call	hex8
+	movw	$msg_readerror,%si	/* Display the read error message */
+	jmp	error
+
+/* LBA: %ax into CHS %ch,%dh,%cl */
+lba2chs:
+	pushw	%bx		/* Save */
+	pushw	%dx
+/* Compute sector */
+	xorw	%dx,%dx
+	movw	$HEAD_SIZE,%bx
+	divw	%bx		/* %dx:%ax / %bx; %ax:quotient, %dx:remainder */
+	incb	%dl
+	movb	%dl,%cl		/* Sector */
+/* Compute head and track */
+	xorw	%dx,%dx
+	movw	$CYLINDER_SIZE,%bx
+	divw	%bx		/* %dx:%ax / %bx */
+	movw	%dx,%bx		/* Save the remainder to %bx */
+	popw	%dx		/* Restore %dx*/
+	movb	%bl,%dh		/* Head */
+	movb	%bl,%ch		/* Track */
+	popw	%bx		/* Restore %bx */
+	ret
+
+
+/* Display error message at %si and then halt. */
+error:
+	call	putstr			/* Display error message */
 
 /* Halt */
 halt:
@@ -54,6 +161,40 @@ putc:
 	int	$0x10		/* Call BIOS, print a character in %al */
 	ret
 
+/* Twiddle a bar */
+twiddle:
+	pushw	%ax
+	pushw	%bx
+	movb	twiddle_index,%al	/* Load index */
+	movw	$twiddle_chars,%bx	/* Address table */
+/* Next char */
+	incb	%al
+	andb	$3,%al
+	movb	%al,twiddle_index	/* Save index for next call */
+	xlatb				/* Get char (%al+%bx) -> %al */
+	call    putc			/* Output it */
+	movb	$8,%al			/* Backspace */
+	call	putc			/* Output it */
+	popw	%bx
+	popw	%ax
+	ret
+
+/* Convert AL to hex char, saving the result to [EDI]. */
+hex8:
+	pushl	%eax
+/* Do upper 4 */
+	shrb	$0x4,%al
+	call	hex8.1
+	popl	%eax
+hex8.1:
+	andb	$0xf,%al	/* Get lower 4 */
+	cmpb	$0xa,%al	/* Convert  : CF=1 if %al < $0xa */
+	sbbb	$0x69,%al	/*  to hex  : %al<-%al - $0x69 - CF */
+	das			/*  digit   : BCD */
+	orb	$0x20,%al	/* To lower case */
+	stosb			/* Save char to %di and inc %di */
+	ret			/* (Recursive) */
+
 
 /* Data section */
 	.data
@@ -62,9 +203,19 @@ putc:
 drive:
 	.byte	0
 
+/* State and set of characters for twiddle function */
+twiddle_index:
+	.byte	0x0
+twiddle_chars:
+	.ascii  "|/-\\"
+
 /* Messages */
 msg_welcome:
 	.asciz	"Welcome!\r\n\nLet's get it started.\r\n\n"
+msg_readerror:
+	.ascii  "Read Error: 0x"
+hex_error:
+	.asciz  "00\r\r"
 
 /*
  * Local variables:
--- src/boot/arch/x86_64/loader.s
+++ src/boot/arch/x86_64/loader.s
@@ -1,6 +1,36 @@
+/*_
+ * Copyright 2013 Scyphus Solutions Co. Ltd.  All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@scyphus.co.jp>
+ */
 
+	.set	KERNEL_SEG,0x0900	/* Memory where to load kernel loader */
+	.set	KERNEL_OFF,0x0000	/*  segment and offset [0900:0000] */
+	.set	KERNEL_SIZE,0x8		/* Size of kernel loader in sectors */
+	.set	SECTOR_SIZE,0x200       /* 0x200 for FD, 0x800 for CD */
+
+	.file	"loader.s"
+
+/* Text section */
+	.text
+
+	.code16			/* 16bit real mode */
 	.globl	loader		/* Entry point */
 
+/* Kernel loader */
 loader:
 	nop
+	jmp	halt
 
+/* Idle process */
+idle:
+	sti
+	hlt
+	cli
+	jmp	idle
+
+/* Halt */
+halt:
+	hlt
+	jmp	halt
