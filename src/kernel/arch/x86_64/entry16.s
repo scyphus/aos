57b32a8a7b4a770f7bfa948910d577a57f22a541
Hirochika Asai <asai@scyphus.co.jp>
2013-07-12 11:55:32 +0900

    Moving the code related to protected mode and long mode from bootmon to kernel.

---
+++ src/kernel/arch/x86_64/entry16.s
@@ -0,0 +1,76 @@
+/*_
+ * Copyright 2013 Scyphus Solutions Co. Ltd.  All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@scyphus.co.jp>
+ */
+
+	.include	"asmconst.h"
+	.file		"entry16.s"
+
+	.globl	kstart
+
+	.text
+	.code16
+
+/* Kernel start */
+/* Starting with the top of a segment; i.e., %ip == 0 */
+kstart:
+	/* Copy %cs to %ds */
+	xorl	%eax,%eax
+	movw	%cs,%ax
+	movw	%ax,%ds
+	/* Check the segment offset */
+	shll	$4,%eax
+	cmpl	$kstart,%eax
+	jne	error
+
+	/* Setup GDT */
+	//movl	$kstart,%ebx
+	//movl	$(gdt-kstart),%edx
+	//movw	%bx,(GDT_CODE32_SEL+2)(%edx)
+	//movw	(GDT_CODE32_SEL+4)(%edx),%ax
+	//shrl	$16,%ebx
+	//orb	%bl,%al
+	//movw	%ax,(GDT_CODE32_SEL+4)(%edx)
+
+	/* Setup temporary IDT and GDT */
+	lidt	idtr-kstart
+	lgdt	gdtr-kstart
+
+	movl	%cr0,%eax
+	orb	$0x1,%al	/* Enable protected mode */
+	movl	%eax,%cr0
+
+	/* Go into protected mode and flush the pipeline */
+	ljmpl	$GDT_CODE32_SEL,$entry32
+
+error:
+halt16:
+	hlt
+	jmp	halt16
+
+
+
+/* Data section */
+	.align	16
+	.data
+
+/* Pseudo interrupt descriptor table */
+idtr:
+	.word	0x0
+	.long	0x0
+
+gdt:
+	.word	0x0,0x0,0x0,0x0		/* Null entry */
+	.word	0xffff,0x0,0x9a00,0xaf	/* Code64 */
+	.word	0xffff,0x0,0x9a00,0xcf	/* Code32 */
+	.word	0xffff,0x0,0x9a00,0x8f	/* Code16 */
+	.word	0xffff,0x0,0x9200,0xcf	/* Data */
+gdt.1:
+/* Pseudo global descriptor table register */
+gdtr:
+	.word	gdt.1-gdt-1		/* Limit */
+	.long	gdt			/* Address */
+
+
