31fec8c49cf8560f4f63d6354b0e2b340870647d
Hirochika Asai <asai@scyphus.co.jp>
2014-06-25 19:51:04 +0900

    Implementing the L2/L3 portion of the network driver.

--- src/drivers/net/net.c
+++ src/drivers/net/net.c
@@ -30,6 +30,63 @@ struct net_nd_entry {
     int state;
 };
 
+struct iphdr {
+    u8 ip_vhl;
+    u8 ip_tos;
+    u16 ip_len;
+    u16 ip_id;
+    u16 ip_off;
+    u8 ip_ttl;
+    u8 ip_proto;
+    u16 ip_sum;
+    u8 ip_src[4];
+    u8 ip_dst[4];
+} __attribute__ ((packed));
+
+struct icmp_hdr {
+    u8 type;
+    u8 code;
+    u16 checksum;
+    u16 ident;
+    u16 seq;
+    // data...
+} __attribute__ ((packed));
+
+struct ip6hdr {
+    u32 ip6_vtf;
+    u16 ip6_len;
+    u8 ip6_next;
+    u8 ip6_limit;
+    u8 ip6_src[16];
+    u8 ip6_dst[16];
+} __attribute__ ((packed));
+
+struct icmp6_hdr {
+    u8 type;
+    u8 code;
+    u16 checksum;
+    // data...
+} __attribute__ ((packed));
+
+
+#define ARP_LIFETIME 300 * 1000
+
+/*
+ * Register an ARP entry
+ */
+int
+net_register_arp(const u8 *ipaddr, const u8 *macaddr)
+{
+    u64 nowms;
+    u64 expire;
+
+    /* Get the current time in milliseconds */
+    nowms = arch_clock_get() / 1000 / 1000;
+    expire = nowms + ARP_LIFETIME;
+
+
+    return -1;
+}
 
 
 
