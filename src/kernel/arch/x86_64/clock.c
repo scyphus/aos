1c46b4eb3e61f0b4ece6130f23cf00c0e852d22b
Hirochika Asai <asai@scyphus.co.jp>
2014-11-11 13:18:30 -1000

    ui: code refactoring

--- src/kernel/arch/x86_64/clock.c
+++ src/kernel/arch/x86_64/clock.c
@@ -1,11 +1,10 @@
 /*_
- * Copyright 2013 Scyphus Solutions Co. Ltd.  All rights reserved.
+ * Copyright (c) 2013 Scyphus Solutions Co. Ltd.
+ * All rights reserved.
  *
  * Authors:
- *      Hirochika Asai  <asai@scyphus.co.jp>
+ *      Hirochika Asai  <asai@jar.jp>
  */
-
-/* $Id$ */
 
 #include <aos/const.h>
 #include "arch.h"
